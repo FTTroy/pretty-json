85384dddf1dfdc61ad5282efc4bbfb742920c5f9
FTTroy <troianofrancesco01@gmail.com>
2025-03-16 11:51:53 +0100

    fix resource access

--- src/main/java/com/github/fttroy/prettyJson/service/PhraseService.java
+++ src/main/java/com/github/fttroy/prettyJson/service/PhraseService.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.IOException;
 
 import static com.github.fttroy.prettyJson.utils.Constants.JSON_EXTENSION;
-import static com.github.fttroy.prettyJson.utils.Constants.JSON_ROOT;
 import static com.github.fttroy.prettyJson.utils.ExcelUtils.createExcel;
 import static com.github.fttroy.prettyJson.utils.RandomUtils.randomWord;
 
@@ -20,7 +19,7 @@ public class PhraseService {
     public String generatePhrase(Reason reason) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         String fileName = reason.getValue().concat(JSON_EXTENSION);
-        File jsonResource = new ClassPathResource(fileName).getFile();
+        String jsonResource = new String(new ClassPathResource(fileName).getInputStream().readAllBytes());
         JsonNode node = mapper.readTree(jsonResource).get(randomWord(reason));
         String phrase = mapper.treeToValue(node, Phrase.class).buildPhrase();
         createExcel(phrase, reason);
